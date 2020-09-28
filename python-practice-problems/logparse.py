f61a0a8d52aaac4d100381b22ffb1bb57d59a682
Igor <azikov@ascsoft.ru>
2020-09-28 14:49:19 +0300

    add solution

--- python-practice-problems/logparse.py
+++ python-practice-problems/logparse.py
@@ -11,7 +11,48 @@
     how long the device was ON, and the time stamp of any ERR conditions.
 """
 
+import sys
+import time
+
+def get_next_event(filename):
+    with open(filename, "r") as datafile:
+        for line in datafile:
+            if "dut: Device State: " in line:
+                line = line.strip()
+                # Parse out the action and timestamp
+                action = line.split()[-1]
+                timestamp = line[:19]
+                yield (action, timestamp)
+
+def compute_time_diff_seconds(start, end):
+    return int(
+        time.mktime(time.strptime(end, "%b %d %H:%M:%S:%f"))
+        - time.mktime(time.strptime(start, "%b %d %H:%M:%S:%f"))
+    )
+
+def extract_data(filename):
+    time_on_started = None
+    errs = []
+    total_time_on = 0
+
+    for action, timestamp in get_next_event(filename):
+        # First test for errs
+        if "ERR" == action:
+            errs.append(timestamp)
+        elif ("ON" == action) and (not time_on_started):
+            time_on_started = timestamp
+        elif ("OFF" == action) and time_on_started:
+            time_on = compute_time_diff_seconds(time_on_started, timestamp)
+            total_time_on += time_on
+            time_on_started = None
+    return total_time_on, errs
 
 if __name__ == "__main__":
-    # TODO: Your code goes here
-    print("There are no unit tests for logparse.")
+    total_time_on, errs = extract_data(sys.argv[1])
+    print(f"Device was on for {total_time_on} seconds")
+    if errs:
+        print("Timestamps of error events:")
+        for err in errs:
+            print(f"\t{err}")
+    else:
+        print("No error events found.")
